76d20ef82305a8e1245e26c7ecd2411959a10f11
Alexspill <almi78@mail.ru>
2021-08-10 19:00:28 +0500

    beginning

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
+  let ml = matrix.length;
+  return ml;
+
+
+
+
+
+
+
+
+
+
+
+
+
   return [];
 }
+
+console.log(towelsort([
+  [ 1, 2, 3 ],
+  [ 4, 5, 6 ],
+  [ 7, 8, 9 ],
+  ]));
