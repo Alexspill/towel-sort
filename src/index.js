9b262bf829f2af3cbbfe0255102f9216175af848
Alexspill <almi78@mail.ru>
2021-08-25 14:46:56 +0500

    Imperative

--- src/index.js
+++ src/index.js
@@ -1,24 +1,22 @@
-module.exports = function towelSort (matrix) {
-  let ml = matrix.length;
-  return ml;
-
-
-
-
-
-
-
-
-
-
-
-
-
-  return [];
-}
-
-console.log(towelsort([
-  [ 1, 2, 3 ],
-  [ 4, 5, 6 ],
-  [ 7, 8, 9 ],
-  ]));
+module.exports = function towelSort (matrix) {
+    let result = [];
+    if (matrix === undefined || matrix.length === 0) {
+      return result;}
+    let ml = matrix.length;
+    let flag = true;
+    for (let mi = 0, mii = 0; mi < ml; mi++) 
+        {   
+            if (flag){
+                for (mii = 0; mii < matrix[mi].length; mii++)
+                {result.push(matrix[mi][mii]);
+                flag = false;}
+            }
+            else {
+              for (mii = matrix[mi].length - 1; mii >= 0; mii--)
+                {result.push(matrix[mi][mii]);
+                  flag = true;}
+            }
+        }
+      return result;
+    }    
+  
